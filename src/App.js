29132a7ba10bcf6aa9c131fc7c30bc305afd922a
jijiny <yoojin9325@naver.com>
2020-06-23 19:16:13 +0900

    Create React Project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
+// 메인 자바스크립트 관리 (HTML문서에서 BOdy태그에 해당하는 내용)
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="gray-background">
+      <img src={logo} lat="logo"/>
+      <h2>Let's develop management system</h2>
     </div>
   );
 }
